67631db55c322f9346e22b3f6f9ab618e54dd712
victorrjimenezz <victorjiru@gmail.com>
2022-02-03 00:30:58 +0100

    Added new terminalDialogView class, which displays a terminal over the current Frame Buffer, and allows for an external class to write lines to the terminal.

---
+++ include/view/terminalDialogView.h
@@ -0,0 +1,57 @@
+//
+// Created by Víctor Jiménez Rugama on 2/2/22.
+//
+
+#ifndef INC_4PT_TERMINALDIALOGVIEW_H
+#define INC_4PT_TERMINALDIALOGVIEW_H
+#include "../../include/utils/AppGraphics.h"
+#include "../../include/view/subView.h"
+#include <string>
+
+class subView;
+class PNG;
+class terminalDialogView : public subView {
+private:
+    int active;
+    int lineSeparator;
+    int basexPos;
+    int baseyPos;
+    int maxyPos;
+    int lineXPAD;
+    int currentLineHeight;
+    FT_Face font;
+    subView * parentView;
+    uint32_t * lastInstanceFrameBuffer;
+    Color textColor{};
+    int viewWidth;
+    int viewHeight;
+    int viewSize;
+    std::string repoURL;
+    Scene2D * mainView;
+    PNG * terminal;
+    void generateDarkenedImage();
+public:
+    static terminalDialogView * mainTerminalDialogView;
+    explicit terminalDialogView(subView * parentView, Scene2D * mainView,const int screenWidth,const int screenHeight, FT_Face font);
+    int openTerminalDialogView(const char * repoURLNew);
+    int closeTerminalDialogView();
+    void writeLine(const char * line);
+    void updateView();
+    void newPage();
+    void pressX(){};
+    void pressCircle();
+    void pressTriangle(){};
+    void pressSquare(){};
+    void arrowUp(){};
+    void arrowDown(){};
+    void arrowRight(){};
+    void arrowLeft(){};
+    subView * getParent();
+    subView * getChild();
+    void deleteChild(){};
+    bool isActive();
+    void langChanged(){};
+    ~terminalDialogView();
+    bool isForRepo(const char * forRepoURL);
+};
+#endif //INC_4PT_TERMINALDIALOGVIEW_H
---
+++ src/view/terminalDialogView.cpp
@@ -0,0 +1,127 @@
+//
+// Created by Víctor Jiménez Rugama on 2/2/22.
+//
+#include "../../include/view/terminalDialogView.h"
+#include "../../include/utils/PNG.h"
+#include "../../include/base.h"
+#include "../../include/utils/logger.h"
+
+terminalDialogView * terminalDialogView::mainTerminalDialogView = nullptr;
+terminalDialogView::terminalDialogView(subView * parentView, Scene2D *mainView, int viewWidth, int viewHeight, FT_Face font) : font(font), repoURL() {
+    this->mainView = mainView;
+    this->viewWidth = viewWidth;
+    this->viewHeight = viewHeight;
+    viewSize= viewWidth * viewHeight;
+    textColor = {255,255,255};
+
+    this->basexPos = viewWidth * (1-TERMINAL_WIDTH)/2;
+    this->baseyPos = viewHeight * (1-TERMINAL_HEIGHT)/2;
+
+    int width = viewWidth*TERMINAL_WIDTH;
+    int height = viewHeight*TERMINAL_HEIGHT;
+
+    this->lineXPAD = basexPos*(1+TERMINAL_TEXT_X_PAD);
+    this->lineSeparator = height/ TERMINAL_LINES;
+
+    this->maxyPos = baseyPos+height-lineSeparator;
+
+    active = false;
+    this->parentView = parentView;
+    mainTerminalDialogView = this;
+
+    std::string terminalPNGDir = DATA_PATH;
+    terminalPNGDir+="assets/images/terminal.png";
+    terminal = new PNG(terminalPNGDir.c_str(),width,height);
+
+    lastInstanceFrameBuffer = (uint32_t *)malloc(sizeof(uint32_t) * viewSize);
+
+    generateDarkenedImage();
+}
+void terminalDialogView::generateDarkenedImage(){
+    for(int i = 0; i < viewSize; i++) {
+        uint32_t * pixel = &lastInstanceFrameBuffer[i];
+        uint8_t col;
+
+        col = pixel[0];
+        uint8_t r = col;
+        if(col != 0)
+            r = col-(100*(col/255));
+
+        col = pixel[1];
+        uint8_t g = col;
+        if(col != 0)
+            g = col-(100*(col/255));
+
+        col = pixel[2];
+        uint8_t b = col;
+        if(col != 0)
+            b = col-(100*(col/255));
+
+        uint8_t a = 0xFF;
+
+        uint32_t newColor = /*0x80000000 +*/ (a << 24) + (r << 16) + (g << 8) + b;
+        *pixel = newColor;
+    }
+}
+void terminalDialogView::updateView() {
+    mainView->overWriteFrameBuffer(lastInstanceFrameBuffer, viewSize);
+}
+
+terminalDialogView::~terminalDialogView() {
+    free(lastInstanceFrameBuffer);
+    delete (terminal);
+}
+
+void terminalDialogView::newPage() {
+    terminal->Draw(lastInstanceFrameBuffer,basexPos,baseyPos,viewWidth,viewHeight);
+    this->currentLineHeight = baseyPos+2*lineSeparator;
+}
+
+void terminalDialogView::writeLine(const char *line) {
+    std::string lineStr = line;
+    Scene2D::DrawText(lastInstanceFrameBuffer,(char *) lineStr.substr(0,MAX_TERMINAL_CHARS).c_str(), font, lineXPAD, currentLineHeight,
+                       textColor, textColor,viewWidth,viewHeight);
+    this->currentLineHeight += lineSeparator;
+    if(currentLineHeight > maxyPos)
+        newPage();
+    if(lineStr.size() > MAX_TERMINAL_CHARS)
+        writeLine(lineStr.substr(MAX_TERMINAL_CHARS,2*MAX_TERMINAL_CHARS).c_str());
+}
+int terminalDialogView::closeTerminalDialogView(){
+    active = false;
+    repoURL = "";
+    return active;
+}
+
+void terminalDialogView::pressCircle() {
+    closeTerminalDialogView();
+}
+
+bool terminalDialogView::isActive() {
+    return active;
+}
+
+subView *terminalDialogView::getParent() {
+    return parentView;
+}
+
+subView *terminalDialogView::getChild() {
+    return nullptr;
+}
+
+int terminalDialogView::openTerminalDialogView(const char * repoURLNew) {
+    if(active)
+        return 0;
+    memcpy(&lastInstanceFrameBuffer[0],&mainView->getCurrentFrameBuffer()[0], sizeof(uint32_t) * viewSize);
+    generateDarkenedImage();
+    newPage();
+    active = true;
+    this->repoURL = repoURLNew;
+    return active;
+}
+
+bool terminalDialogView::isForRepo(const char *forRepoURL) {
+    if(strcasecmp(repoURL.c_str(),forRepoURL) == 0)
+        return active && !repoURL.empty();
+    return false;
+}
