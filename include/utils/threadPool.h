92603846444a46e8b1455e0fbdd49f6ca8a24ef1
victorrjimenezz <victorjiru@gmail.com>
2022-03-23 22:12:29 +0100

    Improved threadPool

--- include/utils/threadPool.h
+++ include/utils/threadPool.h
@@ -5,24 +5,31 @@
 #ifndef INC_4PT_THREADPOOL_H
 #define INC_4PT_THREADPOOL_H
 
-#include <thread>
+#include <pthread.h>
 #include <vector>
 #include <mutex>
 #include <queue>
 
 class threadPool {
 private:
+    typedef void * (*THREADFUNCPTR)(void *);
     static bool shutdown;
+
     static std::mutex queueMutex;
-    static std::mutex threadPoolMutex;
-    static uint32_t maxHardwareThreads;
-    static std::queue<std::function<void()>> jobQueue;
-    static std::vector<std::thread> pool;
-    static void waitForTask();
+    static std::deque<std::function<void()>> jobQueue;
+    static std::vector<pthread_t> pool;
+
+    static std::mutex queueMutexSecondary;
+    static std::deque<std::function<void()>> jobQueueSecondary;
+    static std::vector<pthread_t> secondaryPool;
 public:
     static int init();
     static int term();
-    static int addJob(const std::function<void()>& newJob);
 
+    static int addJob(const std::function<void()>& newJob, bool front = false);
+    static int addJobSecondary(const std::function<void()>& newJob, bool front = false);
+
+    static void waitForTask();
+    static void waitForTaskSecondary();
 };
 #endif //INC_4PT_THREADPOOL_H
--- src/utils/threadPool.cpp
+++ src/utils/threadPool.cpp
@@ -1,43 +1,71 @@
 //
 // Created by Víctor Jiménez Rugama on 2/4/22.
 //
+#include <orbis/libkernel.h>
+#include <thread>
 #include "../../include/utils/threadPool.h"
-#include "../../include/utils/logger.h"
 #include "../../include/base.h"
+#include "../../include/utils/logger.h"
 
-bool threadPool::shutdown = false;
-uint32_t threadPool::maxHardwareThreads = std::thread::hardware_concurrency();
+//TODO TRY GENERATING THREADS AT THAT MOMENT AND THEN IF MAX NUMBER REACHED WAIT
+bool threadPool::shutdown = true;
 
 std::mutex threadPool::queueMutex;
-std::mutex threadPool::threadPoolMutex;
-std::queue<std::function<void()>> threadPool::jobQueue;
-std::vector<std::thread> threadPool::pool;
+std::deque<std::function<void()>> threadPool::jobQueue;
+std::vector<pthread_t> threadPool::pool;
+
+std::mutex threadPool::queueMutexSecondary;
+std::deque<std::function<void()>> threadPool::jobQueueSecondary;
+std::vector<pthread_t> threadPool::secondaryPool;
 
 int threadPool::init() {
-    if(!pool.empty())
-        return 1;
-
     shutdown = false;
-    for(int i=0; i < THREADPOOL_AMOUNT; i++)
-        pool.emplace_back(std::thread(&threadPool::waitForTask));
+    pthread_t newThread;
+    int ret;
+    for(int i=0; i < THREADPOOL_MAIN_POOL_AMOUNT; i++) {
+        ret = pthread_create(&newThread, NULL, (THREADFUNCPTR) &threadPool::waitForTask,NULL);
+        if (ret){
+            LOG << "Error:unable to create thread " << ret;
+        }
+        pool.emplace_back(newThread);
+    }
+
+    for(int i=0; i < THREADPOOL_SECONDARY_POOL_AMOUNT; i++) {
+        ret = pthread_create(&newThread, NULL, (THREADFUNCPTR) &threadPool::waitForTaskSecondary,NULL);
+        if (ret){
+            LOG << "Error:unable to create thread " << ret;
+        }
+        secondaryPool.emplace_back(newThread);
+    }
     return 0;
 }
 
 int threadPool::term() {
-    if(pool.empty())
-        return 1;
+    if(pool.empty() && secondaryPool.empty())
+        return 0;
+
+    shutdown = true; // use this flag in condition.wait
+    int ret;
+    // Join all threads.
+    for (pthread_t threadID : pool)
     {
-        std::unique_lock<std::mutex> lock(threadPoolMutex);
-        shutdown = true; // use this flag in condition.wait
+        ret = pthread_join(threadID, NULL);
+        if (ret) {
+            LOG << "Error when joining " << threadID;
+        }
     }
 
     // Join all threads.
-    for (std::thread &th : pool)
+    for (pthread_t threadID : secondaryPool)
     {
-        th.join();
+        ret = pthread_join(threadID, NULL);
+        if (ret) {
+            LOG << "Error when joining " << threadID;
+        }
     }
 
     pool.clear();
+    secondaryPool.clear();
     return 0;
 }
 
@@ -53,21 +81,61 @@ void threadPool::waitForTask() {
                 break;
             else if(!jobQueue.empty()){
                 job = jobQueue.front();
-                jobQueue.pop();
+                jobQueue.pop_front();
             }
 
         }
         if(job != nullptr){
             job(); // function<void()> type
+        } else {
+            std::this_thread::yield();
         }
     }
 }
 
-int threadPool::addJob(const std::function<void()>& newJob) {
-    if(pool.empty())
+
+void threadPool::waitForTaskSecondary() {
+    while (!shutdown)
+    {
+        std::function<void()> job = nullptr;
+
+        {
+            std::lock_guard<std::mutex> lock(queueMutexSecondary);
+
+            if(shutdown)
+                break;
+            else if(!jobQueueSecondary.empty()){
+                job = jobQueueSecondary.front();
+                jobQueueSecondary.pop_front();
+            }
+
+        }
+        if(job != nullptr){
+            job(); // function<void()> type
+        } else {
+            std::this_thread::yield();
+        }
+    }
+}
+
+int threadPool::addJob(const std::function<void()>& newJob, bool front) {
+    if(shutdown || pool.empty())
         return -1;
-    std::unique_lock<std::mutex> lock(queueMutex);
-    jobQueue.emplace(newJob);
+    std::lock_guard<std::mutex> lock(queueMutex);
+    if(front)
+        jobQueue.emplace_front(newJob);
+    else
+        jobQueue.emplace_back(newJob);
     return 0;
 }
 
+int threadPool::addJobSecondary(const std::function<void()>& newJob, bool front) {
+    if(shutdown || secondaryPool.empty())
+        return -1;
+    std::lock_guard<std::mutex> lock(queueMutexSecondary);
+    if(front)
+        jobQueueSecondary.emplace_front(newJob);
+    else
+        jobQueueSecondary.emplace_back(newJob);
+    return 0;
+}
